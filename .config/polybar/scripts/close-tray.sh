b83f35db9c80f391a109da955818bd121bc0bcb6
kensyc <11352915+kensyc@users.noreply.github.com>
2025-10-27 21:38:58 +0100

    Polybar updates

--- .config/polybar/scripts/close-tray.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-kill -9 $(xprop -name "polybar-tray" _NET_WM_PID | grep -o '[[:digit:]]*')
---
+++ .config/polybar/scripts/wireplumber.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+function get_volume() {
+    wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -o '[0-9.]*' | sed 's/0.//g'
+}
+
+function update_label() {
+    if wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -q 'MUTED'; then
+        echo "%{F#575268}%{F-}"
+    else
+        vol=$(get_volume)
+        echo " $vol%"
+    fi
+}
+
+update_label
+
+while [ "$1" != "" ]; do
+    case $1 in
+    -i | --increase)
+        wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+
+        wpctl get-volume @DEFAULT_AUDIO_SINK@
+
+        exit 0
+        ;;
+    -d | --decrease)
+        wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+        wpctl get-volume @DEFAULT_AUDIO_SINK@
+
+        exit 0
+        ;;
+    -t | --toggle-mute)
+        wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+
+        update_label
+
+        exit 0
+        ;;
+    *)
+        exit 0
+        ;;
+    esac
+done
